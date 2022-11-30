9bf16a11e4b8ed35b579f864b3fd7ed0d8c86cd1
Felipe Fernandes <felipedfe@gmail.com>
2022-11-30 12:28:05 -0300

    corrige o caminho para a pasta do arquivo estático

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const app = express();
 app.use(express.json());
 app.use(express.static('express'));
 
-app.use('/test/jobs/', (_req, res) => {
-  res.sendFile(path.join(__dirname + 'express/index.html'));
+app.use('/', (_req, res) => {
+  res.sendFile(path.join(__dirname + '/test/jobs/index.html'));
 });
 
 const server = http.createServer(app);
