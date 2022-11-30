586d6936c1966cc5c66dbbcfce126c987e14508d
Felipe Fernandes <felipedfe@gmail.com>
2022-11-30 15:25:16 -0300

    renderiza cards de vagas

--- jobs-page/src/App.js
+++ jobs-page/src/App.js
@@ -6,13 +6,22 @@ function App() {
   const [jobsList, setJobsList] = useState([]);
 
   useEffect(() => {
-    // getJobs().then((data => setJobsList(data)));
-    getJobs().then((data => console.log(data)));
+    getJobs().then((data => setJobsList(data.data.jobs)));
+    getJobs().then((data => console.log(data.data.jobs)));
   }, []);
 
   return (
     <div className="App">
-      <h1>Jobs List</h1>
+      {jobsList.map((job) => (
+        <section className="job-container">
+          <h3 className="job-title">{job.jobTitle}</h3>
+          <h5 className="job-companyName">{job.companyName}</h5>
+          <div className="job-description-container">
+            <p dangerouslySetInnerHTML={ { __html: job.jobDescription } }></p>
+          </div>
+          <br></br>
+        </section>
+      ))}
     </div>
   );
 }
